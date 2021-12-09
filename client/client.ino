6edf484cc64ddb62e18de3b19f57bb6214801796
MSMartin <matthewscottmartin2001@gmail.com>
2021-12-08 22:03:36 -0800

    Update client.ino

--- client/client.ino
+++ client/client.ino
@@ -1,4 +1,4 @@
-h#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 
 /*
  * Supported Commands
@@ -17,7 +17,7 @@ h#include <ESP8266WiFi.h>
  * RECALIBRATE
  * HELP
  */
-enum COMMANDS{
+enum COMMAND{
   HELP = 0,
   PIR_DETECTION_ON = 1,
   PIR_DETECTION_OFF = 2,
@@ -25,13 +25,47 @@ enum COMMANDS{
   DOOR_DETECTION_OFF = 4,
   TAMPER_DETECTION_ON = 5,
   TAMPER_DETECTION_OFF = 6,
-  PERIODIC_DETECTION_ON = 7,
-  PERIODIC_DETECTION_OFF = 8,
+  PERIODIC_REPORT_ON = 7,
+  PERIODIC_REPORT_OFF = 8,
   SET_HERTZ = 9,
   REQUEST_DATA = 10,
   REQUEST_CONFIG = 11,
   RESET_TAMPER_FLAG = 12,
   RECALIBRATE = 13
+};
+const static struct {
+    COMMAND val;
+    const char *str;
+}conversion[] = {
+    {HELP, "HELP"},
+    {PIR_DETECTION_ON, "PIR_DETECTION_ON"},
+    {PIR_DETECTION_OFF, "PIR_DETECTION_OFF"},
+    {DOOR_DETECTION_ON, "DOOR_DETECTION_ON"},
+    {DOOR_DETECTION_OFF, "DOOR_DETECTION_OFF"},
+    {TAMPER_DETECTION_ON, "TAMPER_DETECTION_ON"},
+    {TAMPER_DETECTION_OFF, "TAMPER_DETECTION_OFF"},
+    {PERIODIC_REPORT_ON, "PERIODIC_REPORT_ON"},
+    {PERIODIC_REPORT_OFF, "PERIODIC_REPORT_OFF"},
+    {SET_HERTZ, "SET_HERTZ"},
+    {REQUEST_DATA, "REQUEST_DATA"},
+    {REQUEST_CONFIG, "REQUEST_CONFIG"},
+    {RESET_TAMPER_FLAG, "RESET_TAMPER_FLAG"},
+    {RECALIBRATE, "RECALIBRATE"}
+};
+COMMAND strToCommand(String str)
+{
+  char str_array[str.length()];
+  str.toCharArray(str_array, str.length());
+  char* command = strtok(str_array, " ");
+  return charArrToCommand(command);
+}
+COMMAND charArrToCommand (const char* str)
+{
+   int j;
+   for (j = 0;  j < sizeof (conversion) / sizeof (conversion[0]);  ++j)
+       if (!strcmp (str, conversion[j].str))
+           return conversion[j].val;    
+   return conversion[0].val;
 }
 const byte MAX_PACKET_SIZE = 6;
 const short CLIENT_WAIT = 100;
@@ -115,11 +149,13 @@ void loop() {
     connectToServer();
   }
 }
-byte processCommand(String command)
+byte processCommand(String commandStr)
 {
+  byte* packet = NULL;
   byte packetSize = 0;
   byte changeVal = 0;
   byte change = 0;
+  COMMAND command = strToCommand(commandStr);
   switch(command)
   {
     case HELP:
@@ -161,7 +197,7 @@ byte processCommand(String command)
       client.write(change);
       client.write(changeVal);
       break;
-    case PERIODIC_REPORTS_ON:
+    case PERIODIC_REPORT_ON:
       changeVal = 136;
       change = 1;
       client.write(change);
@@ -176,9 +212,12 @@ byte processCommand(String command)
     case SET_HERTZ:
       byte hertz = 2;
       client.write(hertz);
-      Serial.println("Input interval in ms:")
+      Serial.println("Input interval in ms:");
       while(!Serial.available());
-      int interval = atoi(Serial.readString());
+      String intervalString = Serial.readString();
+      char intervalCharArr[intervalString.length()];
+      intervalString.toCharArray(intervalCharArr, intervalString.length());
+      int interval = atoi(intervalCharArr);
       client.write(interval);
       break;
     case REQUEST_DATA:
@@ -190,7 +229,6 @@ byte processCommand(String command)
       break;
     case RECALIBRATE:
       break;
-
     default:
       Serial.println("Unrecognized Command");
   }
